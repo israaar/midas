cfaf7a9eb10807f93a8f96255943550c7fb302c2
Kyle Cutler <kyle1320@gmail.com>
2019-04-01 18:11:51 -0400

    Create initial "NodeCounter" component

---
+++ src/middleman/implementations/components/NodeCounter.java
@@ -0,0 +1,15 @@
+package middleman.implementations.components;
+
+import middleman.interfaces.Component;
+import middleman.interfaces.Message;
+
+class NodeCounter extends Component {
+    public NodeCounter() {
+        // TODO
+    }
+
+    @Override
+    public void onMessageReceived(Message message) {
+        // TODO
+    }
+}
