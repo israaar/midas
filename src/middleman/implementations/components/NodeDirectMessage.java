fb5cd1bcd5a399378dfe6a9decc3fca25768f2db
swrap <8915311+swrap@users.noreply.github.com>
2019-04-08 17:04:16 -0400

    Added in Components

---
+++ src/middleman/implementations/components/NodeDirectMessage.java
@@ -0,0 +1,15 @@
+package middleman.implementations.components;
+
+import middleman.interfaces.Component;
+import middleman.interfaces.Message;
+
+class NodeDirectMessage extends Component {
+    public NodeDirectMessage() {
+        // TODO
+    }
+
+    @Override
+    public void onMessageReceived(Message message) {
+        // TODO
+    }
+}
---
+++ src/middleman/implementations/components/NodeGetLocation.java
@@ -0,0 +1,15 @@
+package middleman.implementations.components;
+
+import middleman.interfaces.Component;
+import middleman.interfaces.Message;
+
+class NodeGetLocation extends Component {
+    public NodeGetLocation() {
+        // TODO
+    }
+
+    @Override
+    public void onMessageReceived(Message message) {
+        // TODO
+    }
+}
