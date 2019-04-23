da1fd88c64aafbaa28f1c28463d4362fce8ac3fb
Kyle Cutler <kyle1320@gmail.com>
2019-04-23 15:40:04 -0400

    Fix TCP code, use all components in TCP Example

--- examples/media/tcp/Server.java
+++ examples/media/tcp/Server.java
@@ -34,7 +34,6 @@ public class Server {
             while (!cancel) {
                 Socket clientConnection = serverSocket.accept();
                 ServerClient serverClient = new ServerClient(clientConnection);
-                serverClient.start();
                 middleman.addMedium(serverClient);
             }
 
@@ -52,9 +51,6 @@ public class Server {
         public ServerClient(Socket socket) {
             this.socket = socket;
             this.thread = new Thread(this::run);
-        }
-
-        public void start() {
             this.thread.start();
         }
 
@@ -89,58 +85,3 @@ public class Server {
         }
     }
 }
-
-// package examples.media.tcp;
-
-// import java.io.ObjectInputStream;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.net.ServerSocket;
-// import java.net.Socket;
-
-// import middleman.MiddleMan;
-// import middleman.interfaces.*;
-
-// public class Server extends Medium {
-//     private Thread thread;
-//     private int port;
-//     private boolean cancel = false;
-
-//     public Server(int port) {
-//         this.port = port;
-//         this.thread = new Thread(this::run);
-//         this.thread.start();
-//     }
-
-//     public void cancel() {
-//         this.cancel = true;
-//         this.thread.interrupt();
-//     }
-
-//     @Override
-//     public void send(Message<?> message) {
-//         // Severs don't send messages
-//     }
-
-//     private void run() {
-//         try (ServerSocket serverSocket = new ServerSocket(port)) {
-//             System.out.println("Server started on port: " + port);
-
-//             while (!cancel) {
-//                 try (
-//                     Socket clientConnection = serverSocket.accept();
-//                     InputStream inputStream = clientConnection.getInputStream();
-//                     ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-//                     ) {
-//                     Message<?> msg = (Message<?>) objectInputStream.readObject();
-//                     System.out.println("Received message: " + msg);
-//                     receive(msg);
-//                 } catch (Exception e) {
-//                     e.printStackTrace();
-//                 }
-//             }
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//     }
-// }
